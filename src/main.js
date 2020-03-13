74e53f64fe9944e0bcc08abbc3a65dba33a0d579
tsuen4 <ht17a045@oecu.jp>
2020-03-13 15:08:44 +0900

    とりあえずデータ取得するように

--- src/main.js
+++ src/main.js
@@ -2,6 +2,21 @@ import Vue from 'vue'
 import App from './App.vue'
 import './registerServiceWorker'
 import router from './router'
+import * as firebase from 'firebase'
+import 'firebase/database'
+
+const firebaseConfig = {
+  apiKey: 'AIzaSyAIv7Vf474NWqeWFoE4ffAb6OIEoPyGdEg',
+  authDomain: 'music-info-collection-bot.firebaseapp.com',
+  databaseURL: 'https://music-info-collection-bot.firebaseio.com',
+  // projectId: 'music-info-collection-bot',
+  storageBucket: 'music-info-collection-bot.appspot.com'
+  // messagingSenderId: '1020277248178',
+  // appId: '1:1020277248178:web:8818e559ecd101e758f9ab',
+  // measurementId: 'G-KEHSD8C8T2'
+}
+
+firebase.initializeApp(firebaseConfig)
 
 Vue.config.productionTip = false
 
---
+++ src/views/ListCollection.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="list-collection">
+    <p>{{ $route.params.guildId }}</p>
+    {{ listData }}
+  </div>
+</template>
+
+<script>
+import firebase from 'firebase/app'
+import 'firebase/database'
+
+export default {
+  data () {
+    return {
+      listData: null,
+      switchService: 'youtube'
+    }
+  },
+  mounted () {
+    firebase.database().ref(`tracks/${this.$route.params.guildId}`).on('value', snapshot => {
+      this.listData = snapshot.val()
+    })
+  }
+}
+</script>
+
+<style>
+</style>
