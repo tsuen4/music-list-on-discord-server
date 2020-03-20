1578ccc34268484a72d74b3411b537b8d248ccd8
tsuen4 <ht17a045@oecu.jp>
2020-03-20 11:56:18 +0900

    カードをコンポーネントに

---
+++ src/components/MusicCard.vue
@@ -0,0 +1,39 @@
+<template>
+  <v-card class="mx-auto ma-2">
+    {{ service }}
+    {{ time }}
+    {{ url }}
+    <!-- {{ item.url }} -->
+    <v-list-item>
+      <v-list-item-content>
+        <!-- <v-list-item-title class="mb-1" v-text="item.title" /> -->
+        <!-- <v-list-item-subtitle v-text="item.uploader" /> -->
+      </v-list-item-content>
+    </v-list-item>
+
+    <v-card-actions>
+      <v-btn :href="url" target="_blank" text small>OPEN</v-btn>
+    </v-card-actions>
+  </v-card>
+</template>
+
+<script>
+export default {
+  props: {
+    url: String,
+    service: String,
+    time: Number
+  },
+  data () {
+    return {
+      test: 'https://www.youtube.com/watch?v=uUvthLpSHrQ&feature=youtu.be'
+    }
+  },
+  mounted () {
+
+  }
+}
+</script>
+
+<style>
+</style>
--- src/components/MusicList.vue
+++ src/components/MusicList.vue
@@ -2,18 +2,10 @@
   <v-container>
     <span id="top" />
     <p>{{ guildId }}: {{ service }}</p>
-    <v-card v-for="(item, index) in listData" :key="index" class="mx-auto ma-2">
-      <v-list-item>
-        <v-list-item-content>
-          <v-list-item-title class="mb-1" v-text="item.title" />
-          <v-list-item-subtitle v-text="item.uploader" />
-        </v-list-item-content>
-      </v-list-item>
-
-      <v-card-actions>
-        <v-btn :href="item.url" target="_blank" text small>OPEN</v-btn>
-      </v-card-actions>
-    </v-card>
+    <template v-for="(item, index) in listData">
+      <!-- {{ item }} -->
+      <MusicCard v-bind="item" :key="index" />
+    </template>
     <span id="bottom" />
   </v-container>
 </template>
@@ -21,8 +13,12 @@
 <script>
 import firebase from 'firebase/app'
 import 'firebase/database'
+import MusicCard from '@/components/MusicCard.vue'
 
 export default {
+  components: {
+    MusicCard
+  },
   props: {
     service: String,
     guildId: String
