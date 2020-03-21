9acf375a6dde189e65b96cc605265eddbaa6c448
tsuen4 <ht17a045@oecu.jp>
2020-03-21 19:21:28 +0900

    ルーターのパス変更

--- src/router/index.js
+++ src/router/index.js
@@ -11,7 +11,7 @@ const routes = [
     component: Home
   },
   {
-    path: '/:guildId/:service',
+    path: '/:guildId/',
     component: () => import(/* webpackChunkName: "about" */ '../views/ListView.vue')
   }
 ]
