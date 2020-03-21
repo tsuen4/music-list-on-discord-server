6bc3f5e0d3cc3de7bf046beb976a09d6d5505af4
tsuen4 <ht17a045@oecu.jp>
2020-03-22 00:20:01 +0900

    express で History モードをいい感じにするやつを導入

--- srv/index.js
+++ srv/index.js
@@ -1,16 +1,17 @@
 const path = require('path')
 const express = require('express')
+const history = require('connect-history-api-fallback')
 const app = express()
-const port = process.env.PORT || 3000
+const port = process.env.PORT || 3200
 
 const getInfo = require('./get-info')
 
+app.use(history())
 app.use(express.static(path.join(__dirname, '../dist')))
 app.use(express.urlencoded({ extended: true }))
 app.use(express.json())
 app.listen(port, () => console.log(`port: ${port}`))
 
-app.get('/api/', (req, res) => res.send('Hello World!!'))
 app.post('/api/get-info', async (req, res) => {
   let data = null
   switch (req.body.service) {
