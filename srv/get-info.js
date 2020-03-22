514732d25d80284439a641ad8c5efb355c828ec9
tsuen4 <ht17a045@oecu.jp>
2020-03-23 01:53:18 +0900

    404 で止まらないように

--- srv/get-info.js
+++ srv/get-info.js
@@ -6,20 +6,24 @@ const OGP = async (url) => {
     description: '',
     thumbnail_url: ''
   }
-  await JSDOM.fromURL(url)
-    .then(dom => {
-      const document = dom.window.document
-      if (document.head.querySelector('[property=og\\:title]')) {
-        data.title = document.head.querySelector('[property=og\\:title]').content
-        data.description = document.head.querySelector('[property=og\\:description]').content
-        data.thumbnail_url = document.querySelector('[property=og\\:image]').content
-      } else if (document.querySelector('[name=og\\:title]')) {
-        data.title = document.querySelector('[name=og\\:title]').content
-        data.description = document.head.querySelector('[name=og\\:description]').content
-        data.thumbnail_url = document.querySelector('[name=og\\:image]').content
-      }
-      // console.log(data)
-    })
+  try {
+    await JSDOM.fromURL(url)
+      .then(dom => {
+        const document = dom.window.document
+        if (document.head.querySelector('[property=og\\:title]')) {
+          data.title = document.head.querySelector('[property=og\\:title]').content
+          data.description = document.head.querySelector('[property=og\\:description]').content
+          data.thumbnail_url = document.querySelector('[property=og\\:image]').content
+        } else if (document.querySelector('[name=og\\:title]')) {
+          data.title = document.querySelector('[name=og\\:title]').content
+          data.description = document.head.querySelector('[name=og\\:description]').content
+          data.thumbnail_url = document.querySelector('[name=og\\:image]').content
+        }
+      })
+  } catch (err) {
+    // console.error(err)
+  }
+  // console.log(data)
   return data
 }
 
